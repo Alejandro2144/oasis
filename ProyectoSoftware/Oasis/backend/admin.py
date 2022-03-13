092cddf9c2e801bee1cbaaf32faae1e131295a23
Alejandro Mc Ewen <mcewenalejandro@gmail.com>
2022-03-13 16:01:47 -0500

    Agregar Informacion de Paciente

--- ProyectoSoftware/Oasis/backend/admin.py
+++ ProyectoSoftware/Oasis/backend/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Usuario, HistoriaClinica
+from .models import Usuario, HistoriaClinica, InformacionPaciente
 
 # Register your models here.
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(InformacionPaciente)
+class InformacionPacienteAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(HistoriaClinica)
--- ProyectoSoftware/Oasis/backend/models.py
+++ ProyectoSoftware/Oasis/backend/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from sklearn.preprocessing import maxabs_scale
 
 # Create your models here.
 
@@ -13,7 +14,27 @@ class Usuario(AbstractUser):
     cargo = models.CharField(max_length=20, choices=Cargos.choices, default = Cargos.Paciente)
 
 
+class InformacionPaciente(models.Model):
+    class TipoAfilliacion(models.TextChoices):
+        Cedula = "CC"
+        TarjetaIdentidad = "TI"
+        Pasaporte = "PA"
+    class Raza(models.TextChoices):
+        Blanco = "Blanco"
+        Negro = "Negro"
+        Mestizo = "Mestizo"
+    nombre = models.CharField(max_length=255)
+    tipoAfilliacion = models.CharField(max_length=2, choices=TipoAfilliacion.choices, default=TipoAfilliacion.Cedula)
+    noAfiliacion = models.BigIntegerField()
+    aseguradora = models.CharField(max_length=255)
+    edad = models.IntegerField()
+    raza = models.CharField(max_length=20, choices=Raza.choices, default=Raza.Blanco)
+    etnicidad = models.EmailField()
+    telefono = models.BigIntegerField()
+
+
 class HistoriaClinica(models.Model):
+    paciente = models.ForeignKey(InformacionPaciente, on_delete=models.CASCADE)
     motivo_de_consulta = models.TextField(null = False, blank = False)
     enfermedad_actual = models.TextField(null = False, blank=False)
     antecedentes_morbidos = models.TextField(null = False, blank = False)
