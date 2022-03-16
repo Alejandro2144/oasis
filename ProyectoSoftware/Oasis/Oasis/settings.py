f4a3b6678a5cd8f39953a0a8bb9385c36e54ba00
Alejandro Mc Ewen <mcewenalejandro@gmail.com>
2022-03-15 22:52:32 -0500

    Poniendo los templates

--- ProyectoSoftware/Oasis/Oasis/settings.py
+++ ProyectoSoftware/Oasis/Oasis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-88%&f38=_(d#l3vj0p_@93)xvv%#+ys+1h*!&!41&fl#mvf2(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend'
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
