11180ab656669d829147ab4b0c7e595d3af0c404
Alejandro Mc Ewen <mcewenalejandro@gmail.com>
2022-03-25 17:42:03 -0500

    Entrega 2

--- ProyectoSoftware/Oasis/backend/views.py
+++ ProyectoSoftware/Oasis/backend/views.py
@@ -50,40 +50,36 @@ def tabla_historias(request):
 
 def historia(request, ID):
     if request.method == 'POST':
-        try:
-            post = request.POST
-            paciente, created = InformacionPaciente.objects.get_or_create(no_afiliacion=post.get('no_afiliacion',0))
-            paciente.nombre=post.get('nombre', 0)
-            paciente.tipo_afiliacion=post.get('tipo_afiliacion', 0)
-            paciente.aseguradora=post.get('aseguradora', 0)
-            paciente.edad=post.get('edad', 0)
-            paciente.raza=post.get('raza', 0)
-            paciente.etnicidad=post.get('etnicidad', 0)
-            paciente.email=post.get('email', 0)
-            paciente.telefono=post.get('telefono', 0)
-            paciente.save()
-            historia = HistoriaClinica.objects.get(pk=ID)
-            historia.paciente = paciente
-            historia.motivo_de_consulta = post.get('motivo_de_consulta', 0)
-            historia.enfermedad_actual = post.get('enfermedad_actual', 0)
-            historia.antecedentes_morbidos = post.get('antecedentes_morbidos', 0)
-            historia.antecedentes_ginecoobstétricos = post.get('antecedentes_ginecoobstétricos', 0)
-            historia.medicamentos = post.get('medicamentos', 0)
-            historia.alergias = post.get('alergias', 0)
-            historia.antecedentes_sociales_personales = post.get('antecedentes_sociales_personales', 0)
-            historia.antecedentes_familiares = post.get('antecedentes_familiares', 0)
-            historia.inmunizaciones = post.get('inmunizaciones', 0)
-            historia.revision_por_sistemas = post.get('revision_por_sistemas', 0)
-            historia.epicrisis = post.get('epicrisis', 0)
-            historia.dia_modificado = date.today()
-            usuario = Usuario.objects.get(pk=request.user.id)
-            historia.medico_encargado = usuario
-            historia.motivo_actualizacion = post.get('motivo_actualizacion', 0)
-            historia.firma = post.get('firma', 0)
-            historia.save()
-            messages.success(request, ('Your movie was successfully added!'))
-        except:
-            messages.error(request, 'Error saving form')
+        post = request.POST
+        paciente, created = InformacionPaciente.objects.get_or_create(no_afiliacion=post.get('no_afiliacion',0))
+        paciente.nombre=post.get('nombre', 0)
+        paciente.tipo_afiliacion=post.get('tipo_afiliacion', 0)
+        paciente.aseguradora=post.get('aseguradora', 0)
+        paciente.edad=post.get('edad', 0)
+        paciente.raza=post.get('raza', 0)
+        paciente.etnicidad=post.get('etnicidad', 0)
+        paciente.email=post.get('email', 0)
+        paciente.telefono=post.get('telefono', 0)
+        paciente.save()
+        historia = HistoriaClinica.objects.get(pk=ID)
+        historia.paciente = paciente
+        historia.motivo_de_consulta = post.get('motivo_de_consulta', 0)
+        historia.enfermedad_actual = post.get('enfermedad_actual', 0)
+        historia.antecedentes_morbidos = post.get('antecedentes_morbidos', 0)
+        historia.antecedentes_ginecoobstétricos = post.get('antecedentes_ginecoobstétricos', 0)
+        historia.medicamentos = post.get('medicamentos', 0)
+        historia.alergias = post.get('alergias', 0)
+        historia.antecedentes_sociales_personales = post.get('antecedentes_sociales_personales', 0)
+        historia.antecedentes_familiares = post.get('antecedentes_familiares', 0)
+        historia.inmunizaciones = post.get('inmunizaciones', 0)
+        historia.revision_por_sistemas = post.get('revision_por_sistemas', 0)
+        historia.epicrisis = post.get('epicrisis', 0)
+        historia.dia_modificado = date.today()
+        usuario = Usuario.objects.get(pk=request.user.id)
+        historia.medico_encargado = usuario
+        historia.motivo_actualizacion = post.get('motivo_actualizacion', 0)
+        historia.firma = post.get('firma', 0)
+        historia.save()
         return redirect("/historias")
     historia = get_object_or_404(HistoriaClinica, pk=ID)
     return render(request, 'historias/historia.html', {'historia': historia, 'tipos' : InformacionPaciente.TipoAfilliacion,'razas' : InformacionPaciente.Raza})
@@ -134,12 +130,9 @@ def crear_historia(request):
             dia_creado = date.today(),
             dia_modificado = date.today(),
             medico_encargado = usuario,
-            motivo_actualizacion = post.get('motivo_actualizacion', 0),
+            motivo_actualizacion = "",
             firma = post.get('firma', 0)
         )
-        messages.success(request, ('Se creó tu historia'))
-        # except:
-        #     messages.error(request, 'Error creando historia de usuario')
         return redirect("/historias")
     historia = {'pk': 'crear'}
     return render(request, 'historias/historia.html', {'historia': historia, 'tipos' : InformacionPaciente.TipoAfilliacion,'razas' : InformacionPaciente.Raza})
